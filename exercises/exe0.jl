5dbffb5ab79d4f284c79d45f670efa9fe93dc2b3
אבי בן יקר <aviby0@gmail.com>
2019-03-12 22:11:30 +0200

    exe 0 work

--- exercises/exe0.jl
+++ exercises/exe0.jl
@@ -1,67 +1,41 @@
 
 
 function exe0(dirpath)
-dirpath="C:\\Users\\AVI\\.atom\\julia-projects\\exercises\\exercise_0"
-cwd=joinpath(dirpath,"hello.in")
-    if ispath(cwd)
+cwd=joinpath(dirpath,"hello.in")#make path to hello
+    if ispath(cwd)#cheak if hello.in exist
         try
-            open(cwd)do f
-                open("exemple.text","w")do writeFile
-                    write(writeFile,f)
-                end
-            end
-            open("exemple.text")do file
-                for i in readlines(file)
+            open(cwd)do f #open hello.in
+                open("exemple.text","w")do writeFile #open (or creat if not exist) exemple.text at the current directory to write
+                    write(writeFile,f) #write hello.in to exemple.text
+                end#open exemple
+            end#open cwd
+            open("exemple.text")do file #print every line from exemple that contain the word 'you'
+                for i in readlines(file)#read all the file to arry by line
                     if occursin("you",i)
                     write(stdout,i,"\n")
-                end
-                end
-            end
+                end#if occursin
+            end #for readlines
+        end#open exemple
         catch exc
             println(exc)
-        end
-    end
-    for i in readdir(dirpath)
-        if isfile(i)
-            try
+        end#catch
+    end#if ispath
+    for i in readdir(dirpath) #foreach file or dir in dirpath
+        if !isdir(i)
             f=joinpath(dirpath,i)
-            println()
-            txt=""
-            open(f,"r")do arg
-                global txt=readlines(arg)
-            end
-            breakpoint("3")
+            txt="" #make var for the contain of the file
+            open(f,"r")do arg #read all the file to array
+                txt=readlines(arg)
+            end#open f
             open(f,"w")do args
-                for i in enumerate(txt)
-                    write(args,"$(i[1]). $(i[2])\n")
-                end
-            end
-            catch exc
-                println(exc)
-            end
-        end
+                for i in enumerate(txt) #enumerate itearate on the file
+                    write(args,"$(i[1]). $(i[2])\n")#the first element is the num in the itertor
+                end#for enumerate
+            end#open f
 
-    end
-end
-#=function recExe0()
+        end#if !isdir
 
-    for x in readdir(pwd())
-            if isdir(x)
-                exe0(x)
-            end
-    end
+    end#for readdir
+end#function exe0
 
-end
-=#
-#recExe0()
-#=function test()
-try
-    f=open("C:\\Users\\AVI\\.atom\\julia-projects\\exercises\\exercise_0","w")
-    print("f")
-    close(f)
-catch e
-    println(e)
-end
-end
-=#
 exe0("C:\\Users\\AVI\\.atom\\julia-projects\\exercises\\exercise_0")
